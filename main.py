a3757a742aeb9fb9434d75f00a471a59b4a9892f
Jingai Atari <Kouno-San2020@outlook.jp>
2025-07-22 23:34:08 +0900

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-def on_forever():
+A = 0
+
+def on_gesture_eight_g():
     pass
+input.on_gesture(Gesture.EIGHT_G, on_gesture_eight_g)
+
+def on_button_pressed_a():
+    global A
+    A = 0
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_every_interval():
+    pass
+loops.every_interval(500, on_every_interval)
+
+def on_forever():
+    pins.digital_write_pin(DigitalPin.P1, 1)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function on_forever() {
+let A = 0
+input.onGesture(Gesture.EightG, function on_gesture_eight_g() {
     
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    A = 0
+})
+loops.everyInterval(500, function on_every_interval() {
+    
+})
+basic.forever(function on_forever() {
+    pins.digitalWritePin(DigitalPin.P1, 1)
 })
