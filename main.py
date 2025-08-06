e932269d3be7e48096fac8071aa30608602753cf
Jingai Atari <Kouno-San2020@outlook.jp>
2025-08-06 18:57:06 +0900

    Add external LED blinking feature and fix Turn_Left function duplication

--- main.py
+++ main.py
@@ -1,76 +1,93 @@
-A = 0
+blink = 0
+reset_flag = 0
 def Turn_Right():
-    basic.show_leds("""
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        """)
-    basic.pause(10)
-    basic.show_leds("""
-        # # # # #
-        # # # # #
-        # # # # #
-        . . . . .
-        . . . . .
-        """)
-    basic.pause(10)
-    basic.show_leds("""
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        """)
-    basic.pause(10)
-    basic.show_arrow(ArrowNames.NORTH)
-    basic.pause(10)
-
-def on_gesture_eight_g():
-    pass
-input.on_gesture(Gesture.EIGHT_G, on_gesture_eight_g)
+    basic.clear_screen()
+    for index in range(4):
+        Turn_Right_Anime()
+    for index2 in range(2):
+        basic.show_arrow(ArrowNames.NORTH)
+        basic.pause(10)
+        basic.clear_screen()
+        basic.pause(500)
 
 def on_button_pressed_a():
-    global A
-    A = 0
+    global blink
+    basic.clear_screen()
+    blink = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_gesture_logo_up():
-    Turn_Left()
+    global reset_flag, blink
+    reset_flag = 1
+    blink = 1
+    while not (input.is_gesture(Gesture.SCREEN_UP)):
+        Turn_Left()
+    reset_flag = 0
+    blink = 0
+    basic.clear_screen()
 input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
 
-def Turn_Left():
-    basic.show_leds("""
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        """)
-    basic.pause(10)
-    basic.show_leds("""
-        # # # . .
-        # # # . .
-        # # # . .
-        # # # . .
-        # # # . .
-        """)
-    basic.pause(10)
-    basic.show_leds("""
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        """)
-    basic.pause(10)
-    basic.show_arrow(ArrowNames.WEST)
-    basic.pause(10)
+def on_gesture_screen_up():
+    basic.clear_screen()
+    if reset_flag:
+        control.reset()
+input.on_gesture(Gesture.SCREEN_UP, on_gesture_screen_up)
+
+def Turn_Left_Anime():
+    Toggle_all_of_column(4, False)
+    Toggle_all_of_column(3, False)
+    Toggle_all_of_column(2, False)
+    Toggle_all_of_column(1, False)
+    Toggle_all_of_column(0, True)
+    Toggle_all_of_column(4, True)
+    Toggle_all_of_column(3, True)
+    Toggle_all_of_column(2, True)
+    Toggle_all_of_column(1, True)
+    Toggle_all_of_column(0, True)
+    basic.pause(100)
+def Turn_Right_Anime():
+    Toggle_all_of_row(4, False)
+    Toggle_all_of_row(3, False)
+    Toggle_all_of_row(2, False)
+    Toggle_all_of_row(1, False)
+    Toggle_all_of_row(0, True)
+    Toggle_all_of_row(4, True)
+    Toggle_all_of_row(3, True)
+    Toggle_all_of_row(2, True)
+    Toggle_all_of_row(1, True)
+    Toggle_all_of_row(0, True)
+    basic.pause(100)
 
 def on_gesture_tilt_right():
-    Turn_Right()
+    global reset_flag, blink
+    reset_flag = 1
+    blink = 1
+    while not (input.is_gesture(Gesture.SCREEN_UP)):
+        Turn_Right()
+    reset_flag = 0
+    blink = 0
+    basic.clear_screen()
 input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
+
+def Turn_Left():
+    basic.clear_screen()
+    for index3 in range(4):
+        Turn_Left_Anime()
+    for index4 in range(2):
+        basic.show_arrow(ArrowNames.WEST)
+        basic.pause(10)
+        basic.clear_screen()
+        basic.pause(500)
+def Toggle_all_of_column(column: number, WithPause: bool):
+    for i in range(5):
+        led.toggle(column, i)
+    if WithPause:
+        basic.pause(100)
+def Toggle_all_of_row(Row: number, WithPause2: bool):
+    for j in range(5):
+        led.toggle(j, Row)
+    if WithPause2:
+        basic.pause(50)
 
 def on_every_interval():
     pass
@@ -81,5 +98,9 @@ def on_forever():
 basic.forever(on_forever)
 
 def on_forever2():
-    pass
+    if blink:
+        pins.digital_write_pin(DigitalPin.P1, 1)
+        basic.pause(300)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        basic.pause(300)
 basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,75 +1,113 @@
-let A = 0
+let blink = 0
+let reset_flag = 0
 function Turn_Right() {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(10)
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(10)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(10)
-    basic.showArrow(ArrowNames.North)
-    basic.pause(10)
+    basic.clearScreen()
+    for (let index = 0; index < 4; index++) {
+        Turn_Right_Anime()
+    }
+    for (let index2 = 0; index2 < 2; index2++) {
+        basic.showArrow(ArrowNames.North)
+        basic.pause(10)
+        basic.clearScreen()
+        basic.pause(500)
+    }
 }
 
-input.onGesture(Gesture.EightG, function on_gesture_eight_g() {
-    
-})
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    A = 0
+    basic.clearScreen()
+    blink = 0
 })
 input.onGesture(Gesture.LogoUp, function on_gesture_logo_up() {
-    Turn_Left()
+    
+    reset_flag = 1
+    blink = 1
+    while (!input.isGesture(Gesture.ScreenUp)) {
+        Turn_Left()
+    }
+    reset_flag = 0
+    blink = 0
+    basic.clearScreen()
 })
-function Turn_Left() {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(10)
-    basic.showLeds(`
-        # # # . .
-        # # # . .
-        # # # . .
-        # # # . .
-        # # # . .
-        `)
-    basic.pause(10)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(10)
-    basic.showArrow(ArrowNames.West)
-    basic.pause(10)
+input.onGesture(Gesture.ScreenUp, function on_gesture_screen_up() {
+    basic.clearScreen()
+    if (reset_flag) {
+        control.reset()
+    }
+    
+})
+function Turn_Left_Anime() {
+    Toggle_all_of_column(4, false)
+    Toggle_all_of_column(3, false)
+    Toggle_all_of_column(2, false)
+    Toggle_all_of_column(1, false)
+    Toggle_all_of_column(0, true)
+    Toggle_all_of_column(4, true)
+    Toggle_all_of_column(3, true)
+    Toggle_all_of_column(2, true)
+    Toggle_all_of_column(1, true)
+    Toggle_all_of_column(0, true)
+    basic.pause(100)
+}
+
+function Turn_Right_Anime() {
+    Toggle_all_of_row(4, false)
+    Toggle_all_of_row(3, false)
+    Toggle_all_of_row(2, false)
+    Toggle_all_of_row(1, false)
+    Toggle_all_of_row(0, true)
+    Toggle_all_of_row(4, true)
+    Toggle_all_of_row(3, true)
+    Toggle_all_of_row(2, true)
+    Toggle_all_of_row(1, true)
+    Toggle_all_of_row(0, true)
+    basic.pause(100)
 }
 
 input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
-    Turn_Right()
+    
+    reset_flag = 1
+    blink = 1
+    while (!input.isGesture(Gesture.ScreenUp)) {
+        Turn_Right()
+    }
+    reset_flag = 0
+    blink = 0
+    basic.clearScreen()
 })
+function Turn_Left() {
+    basic.clearScreen()
+    for (let index3 = 0; index3 < 4; index3++) {
+        Turn_Left_Anime()
+    }
+    for (let index4 = 0; index4 < 2; index4++) {
+        basic.showArrow(ArrowNames.West)
+        basic.pause(10)
+        basic.clearScreen()
+        basic.pause(500)
+    }
+}
+
+function Toggle_all_of_column(column: number, WithPause: boolean) {
+    for (let i = 0; i < 5; i++) {
+        led.toggle(column, i)
+    }
+    if (WithPause) {
+        basic.pause(100)
+    }
+    
+}
+
+function Toggle_all_of_row(Row: number, WithPause2: boolean) {
+    for (let j = 0; j < 5; j++) {
+        led.toggle(j, Row)
+    }
+    if (WithPause2) {
+        basic.pause(50)
+    }
+    
+}
+
 loops.everyInterval(500, function on_every_interval() {
     
 })
@@ -77,5 +115,11 @@ basic.forever(function on_forever() {
     
 })
 basic.forever(function on_forever2() {
+    if (blink) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(300)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(300)
+    }
     
 })
